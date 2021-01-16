c851f906eb29f6be64af90141e7c8770a1844ca6
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2021-01-16 13:55:59 +0530

    modified console.log

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 
 
-console.log('Client side js running')
+console.log('Client side js is running')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
